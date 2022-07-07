07c11a1f3ad32760660a660d8e2db015fa9f384d
Nawfal5 <106992185+Nawfal5@users.noreply.github.com>
2022-07-07 20:03:53 +0100

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
-
+    
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
